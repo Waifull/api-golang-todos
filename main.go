61c7a673e0ee60cd4923843a4c98b42cef142fe7
Wafiul Achdi <achdiwafiul@gmail.com>
2025-05-03 18:39:45 +0700

    Initial commit: upload API todos dengan Echo

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+
+	"golang-todos/controller"
+	"golang-todos/database"
+
+	"github.com/labstack/echo"
+)
+
+
+
+
+
+func main() {
+	
+	db := database.InitDb()
+	defer db.Close()
+
+	err := db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	e := echo.New()
+
+	controller.NewGetAllTodosController(e, db)
+	controller.NewCreateTodoController(e, db)
+	controller.NewDeleteTodoController(e, db)
+	controller.NewUpdateTodoController(e, db)
+	controller.NewCheckTodoController(e, db)
+	
+	
+	e.Start(":8080")
+}
